9609480b3e8daafcf1a6553e224d4d521505771b
Akash Ur Rehman <fa19-bse-112@cuilahore.edu.pk>
2023-02-10 23:33:07 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 function App() {
   return (
-    <div className="App" style={{textalign:"center", justifyContent:"center", marginHorizontal:"10renm"}}>
-      <a href="/https://github.com/akashurrehman">
-        Visit the Github Profile
+    <div style={{textalign:"center", justifyContent:"center", marginHorizontal:"10renm"}}>
+      <a href="https://github.com/akashurrehman" target="_blank" rel="noopener noreferrer">
+          Go to Github
       </a>
+
     </div>
   );
 }
